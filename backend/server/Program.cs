76b55d90f91333bc4170b6cb26ff716c7c1e7033
ASH <a.shakirov@forexdevelopment.com>
2020-07-07 16:36:07 +0500

    added server app

--- backend/server/Program.cs
+++ backend/server/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace server
+namespace Server
 {
     public class Program
     {
