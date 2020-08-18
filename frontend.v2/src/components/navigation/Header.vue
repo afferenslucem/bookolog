5ae1e0f71fd339c66bd142edb65b92f50f64d2c6
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-18 12:00:51 +0500

    started

---
+++ frontend.v2/src/components/navigation/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+        <div class="icon">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+        </div>
+        <div class="icon">
+            <i class="fa fa-user-o" aria-hidden="true"></i>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- frontend.v2/src/views/Home.vue
+++ frontend.v2/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Header class="top">
+  </Header>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/navigation/Header'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
