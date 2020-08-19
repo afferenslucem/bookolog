8d353a7d574ab056ffcc74f9344a0fcfdcb46305
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-19 08:59:50 +0500

    moved styles

--- frontend.v2/src/components/navigation/Header.vue
+++ frontend.v2/src/components/navigation/Header.vue
@@ -29,3 +29,21 @@ export default {
     },
 }
 </script>
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.header {
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    > div {
+        flex: 0 0 auto;
+    }
+
+    background-color: $header-color;
+    color: $header-text-color;
+}
+</style>
