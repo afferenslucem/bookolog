1a29473313d344468fb3046284e8afb5ee5a0dd8
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-19 15:38:28 +0500

    added book getters

--- frontend.v2/src/models/book.js
+++ frontend.v2/src/models/book.js
@@ -1,10 +1,10 @@
-export const PAPER_BOOK = 0;
-export const ELECTRONIC_BOOK = 1;
-export const AUDIO_BOOK = 2;
+export const PAPER_BOOK_TYPE = 0;
+export const ELECTRONIC_BOOK_TYPE = 1;
+export const AUDIO_BOOK_TYPE = 2;
 
-export const TO_READ = 0;
-export const IN_PROGRESS = 1;
-export const DONE = 2;
+export const TO_READ_STATUS = 0;
+export const IN_PROGRESS_STATUS = 1;
+export const DONE_STATUS = 2;
 
 export class Book {
     // name: string
--- frontend.v2/src/store/book/getters.js
+++ frontend.v2/src/store/book/getters.js
@@ -1,5 +1,9 @@
-import {USER_LOGGED_IN_GETTER} from '../naming';
+import {BOOKS_IN_PROGRESS_GETTER, BOOKS_TO_READ_GETTER, BOOKS_DONE_GETTER} from '../naming';
+import _ from 'ursus-utilus-collections';
+import {IN_PROGRESS_STATUS, TO_READ_STATUS, DONE_STATUS} from '@/models/book';
 
 export const getters = {
-    [USER_LOGGED_IN_GETTER]: state => state.login !== ''
+    [BOOKS_IN_PROGRESS_GETTER]: state => _(state.books).where(item => item.status === IN_PROGRESS_STATUS).toArray(),
+    [BOOKS_TO_READ_GETTER]: state => _(state.books).where(item => item.status === TO_READ_STATUS).toArray(),
+    [BOOKS_DONE_GETTER]: state => _(state.books).where(item => item.status === DONE_STATUS).toArray(),
 }
