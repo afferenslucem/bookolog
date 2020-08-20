dca360a6121abc6fd13c567b0d8d81780683e934
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-20 08:57:06 +0500

    added reset css and books list

--- frontend.v2/src/views/BooksInProgressList.vue
+++ frontend.v2/src/views/BooksInProgressList.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-      Hi, i'm ready
+      <ul v-if="shouldShowList">
+        <li v-for="book of books" :key="book.guid">
+          <span>{{book.name}}</span>
+        </li>
+      </ul>
+      <div v-else>
+        Ты ничего не читаешь
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import {BOOKS_IN_PROGRESS_GETTER} from '@/store/naming';
 
+export default {
+  computed: {
+    books() {
+      return this.$store.getters[BOOKS_IN_PROGRESS_GETTER];
+    },
+    shouldShowList() {
+      return (this.books != null) && (this.books.length > 0)
+    }
+  }
 }
 </script>
 
