2abf6220430da61d687341935a1ced7ee2b9a3f4
root <alexshakirov74@gmail.com>
2020-08-22 19:43:22 +0500

    added form link

--- frontend.v2/src/components/navigation/BookMenu.vue
+++ frontend.v2/src/components/navigation/BookMenu.vue
@@ -1,13 +1,13 @@
 <template>
   <ul class="nav flex-column">
     <li class="nav-item">
-      <a class="nav-link" href="#" @click="goToInProgress()">Читаю Сейчас</a>
+      <a class="nav-link" href="#" @click="goToInProgress()">Читаю Сейчас</a> <a class="icon nav-link" href="#"><i class="fa fa-lg fa-plus-square-o" aria-hidden="true"></i></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#" @click="goToToRead()">К Прочтению</a>
+      <a class="nav-link" href="#" @click="goToToRead()">К Прочтению</a> <a class="icon nav-link" href="#"><i class="fa fa-lg fa-plus-square-o" aria-hidden="true"></i></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#" @click="goToDone()">Прочитал</a>
+      <a class="nav-link" href="#" @click="goToDone()">Прочитал</a> <a class="icon nav-link" href="#"><i class="fa fa-lg fa-plus-square-o" aria-hidden="true"></i></a>
     </li>
   </ul>
 </template>
@@ -33,3 +33,16 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    .icon {
+      margin-right: .5rem;
+      padding: 0;
+    }
+  }
+</style>
