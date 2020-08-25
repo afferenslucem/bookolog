eeffecd8acfa146f1340d18c0988886e8da7dec8
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-25 16:22:47 +0500

    added tests

---
+++ frontend.v2/tests/unit/store/book/mutations.spec.js
@@ -0,0 +1,28 @@
+import { mutations } from '@/store/book/mutations'
+import { BOOKS_SAVE_MUTATION, BOOK_ADD_MUTATION } from '@/store/naming'
+import books from '../../data/books'
+import { assert } from 'chai';
+
+describe('Book Mutations', () => {
+    it('should save books', () => {
+        const state = {
+        };
+
+        mutations[BOOKS_SAVE_MUTATION](state, books);
+
+        const expected = books;
+
+        assert.deepEqual(state.books, expected)
+    })
+    it('should add books', () => {
+        const state = {
+            books: []
+        };
+
+        mutations[BOOK_ADD_MUTATION](state, books[0]);
+
+        const expected = books[0];
+
+        assert.deepEqual(state.books[0], expected)
+    })
+})
