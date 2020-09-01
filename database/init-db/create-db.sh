5cd8d8c8523be75b73aacab69b277bb0438fcebc
ASH <alexshakirov74@gmail.com>
2020-09-01 17:47:15 +0500

    fixed scripts

---
+++ database/init-db/create-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    create user bookolog with password 'masterkey';
+    create database bookolog;
+    grant all privileges on database bookolog to bookolog;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname bookolog <<-EOSQL
+    create extension if not exists "uuid-ossp";
+EOSQL
