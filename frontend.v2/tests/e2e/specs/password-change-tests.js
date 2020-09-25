e503fbee6a18bd654e14c523d0f0df0b1ac7f0c5
Alex Shakirov <a.shakirov@infinnity.ru>
2020-09-25 14:39:06 +0500

    added new pc tests

--- frontend.v2/tests/e2e/specs/password-change-tests.js
+++ frontend.v2/tests/e2e/specs/password-change-tests.js
@@ -15,6 +15,40 @@ describe('Password change test', () => {
   })
 
   const passwordChange = credentials.passwordChangeAccount;
+ 
+  it('Wrong Password Change', () => {
+    cy.login(passwordChange.username, passwordChange.password);
+
+    cy.pageIs('workspace/in-progress');
+    cy.get('.profile').should('exist')
+
+    cy.goToSettings();
+    cy.pageIs('workspace/settings');
+
+    cy.changePassword('123', '12345678')
+
+    cy.get('[name="password-change"] [type="submit"]').click();
+    
+    cy.get('.alert-danger').contains('Неверный пароль.');
+  });
+ 
+  it('Different Passwords Change', () => {
+    cy.login(passwordChange.username, passwordChange.password);
+
+    cy.pageIs('workspace/in-progress');
+    cy.get('.profile').should('exist')
+
+    cy.goToSettings();
+    cy.pageIs('workspace/settings');
+
+    cy.changePassword(passwordChange.password, '12345678')
+
+    cy.get(`#confirmation-change`).type('123');
+
+    cy.get('[name="password-change"] [type="submit"]').click();
+
+    cy.get('.alert-danger').contains('Пароли не совпадают.');
+  });
 
   it('Password change test', () => {
     cy.login(passwordChange.username, passwordChange.password);
@@ -28,8 +62,6 @@ describe('Password change test', () => {
     cy.changePassword(passwordChange.password, '12345678')
 
     cy.get('[name="password-change"] [type="submit"]').click();
-
-    cy.wait(1000);
 
     cy.logout();
 
