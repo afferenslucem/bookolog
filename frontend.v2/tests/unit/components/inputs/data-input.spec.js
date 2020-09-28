3793b39f0210ce72cac0872e7304182d43e637d4
Alex Shakirov <a.shakirov@infinnity.ru>
2020-09-28 15:35:49 +0500

    added tests

--- frontend.v2/tests/unit/components/inputs/data-input.spec.js
+++ frontend.v2/tests/unit/components/inputs/data-input.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import DateInput from '../../../../src/components/inputs/BookDateInput'//'@/components/inputs/BookDateInput.vue';
+import DateInput from '@/components/inputs/BookDateInput'//'@/components/inputs/BookDateInput.vue';
 
 describe('BookDataInput.vue', () => {
     it('Renders empty props for nulls', () => {
