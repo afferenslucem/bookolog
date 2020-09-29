321f83a1504445ca7fb2938cf9be2725191fa07c
Alex Shakirov <a.shakirov@infinnity.ru>
2020-09-29 16:44:55 +0500

    added prefill functionality

--- frontend.v2/src/views/books/entity/BookUpdate.vue
+++ frontend.v2/src/views/books/entity/BookUpdate.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <small class="dark-text">* {{ $t('book.form.titles.required') }}</small>
+    <small class="dark-text">* {{ $t("book.form.titles.required") }}</small>
     <form class="needs-validation" @submit="submit($event)">
       <div class="form-group">
         <input
@@ -8,7 +8,7 @@
           name="name"
           id="name"
           class="form-control"
-          :placeholder="`${ $t('book.form.titles.name') }*`"
+          :placeholder="`${$t('book.form.titles.name')}*`"
           :pattern="bookNamePattern"
           required
           v-model.trim="book.name"
@@ -26,11 +26,19 @@
           :pattern="bookAuthorsPattern"
           v-model.trim="authorsComp"
         />
-        <small id="authorsInput" class="text-muted">{{ $t('book.form.titles.splitByComma') }}</small>
+        <small id="authorsInput" class="text-muted">{{
+          $t("book.form.titles.splitByComma")
+        }}</small>
       </div>
       <div class="form-group">
-        <input type="number" name="year" id="year" class="form-control"
-        v-model.number="book.year" :placeholder="$t('book.form.titles.year')" />
+        <input
+          type="number"
+          name="year"
+          id="year"
+          class="form-control"
+          v-model.number="book.year"
+          :placeholder="$t('book.form.titles.year')"
+        />
       </div>
       <div class="form-group">
         <input
@@ -57,21 +65,48 @@
         <small id="tagsInput" class="text-muted">Вводите через запятую</small>
       </div>
       <div class="form-group">
-        <label for="status">{{ $t('book.form.titles.status') }}</label>
-        <select class="form-control" name="status" id="status" v-model.number="book.status">
-          <option v-for="option in statuses" :key="option.value" :value="option.value">{{option.name}}</option>
+        <label for="status">{{ $t("book.form.titles.status") }}</label>
+        <select
+          class="form-control"
+          name="status"
+          id="status"
+          v-model.number="book.status"
+          @change="statusChange()"
+        >
+          <option
+            v-for="option in statuses"
+            :key="option.value"
+            :value="option.value"
+          >
+            {{ option.name }}
+          </option>
         </select>
       </div>
       <div class="form-group">
-        <label for="type">{{ $t('book.form.titles.type') }}</label>
-        <select class="form-control" name="type" id="type" v-model.number="book.type">
-          <option v-for="option in bookTypes" :key="option.value" :value="option.value">{{option.name}}</option>
+        <label for="type">{{ $t("book.form.titles.type") }}</label>
+        <select
+          class="form-control"
+          name="type"
+          id="type"
+          v-model.number="book.type"
+        >
+          <option
+            v-for="option in bookTypes"
+            :key="option.value"
+            :value="option.value"
+          >
+            {{ option.name }}
+          </option>
         </select>
       </div>
-      <label for="progress" v-show="showProgress">{{progressHeader}}</label>
+      <label for="progress" v-show="showProgress">{{ progressHeader }}</label>
       <div class="row progress-row form-group" v-show="showProgress">
         <div class="col-5" id="progress">
-          <audio-book-units-input id="doneUnits" v-if="book.type === 2" :units.sync="book.doneUnits"></audio-book-units-input>
+          <audio-book-units-input
+            id="doneUnits"
+            v-if="book.type === 2"
+            :units.sync="book.doneUnits"
+          ></audio-book-units-input>
           <input
             v-else
             type="number"
@@ -83,9 +118,13 @@
             :max="Math.min(book.totalUnits, maxUnitsCount)"
           />
         </div>
-        <div class="col-2 from">{{ $t('book.form.titles.progress.from') }}</div>
+        <div class="col-2 from">{{ $t("book.form.titles.progress.from") }}</div>
         <div class="col-5">
-          <audio-book-units-input id="totalUnits" v-if="book.type === 2" :units.sync="book.totalUnits"></audio-book-units-input>
+          <audio-book-units-input
+            id="totalUnits"
+            v-if="book.type === 2"
+            :units.sync="book.totalUnits"
+          ></audio-book-units-input>
           <input
             v-else
             type="number"
@@ -102,23 +141,40 @@
       <div class="row form-group">
         <div class="col-12 col-md-6" v-show="showStartDate">
           <div class="form-group start-date">
-            <label for="startDate">{{ $t('book.form.titles.started') }}</label>
-            <date-input :year.sync="book.startDateYear" :month.sync="book.startDateMonth" :day.sync="book.startDateDay"></date-input>
+            <label for="startDate">{{ $t("book.form.titles.started") }}</label>
+            <date-input
+              :year.sync="book.startDateYear"
+              :month.sync="book.startDateMonth"
+              :day.sync="book.startDateDay"
+            ></date-input>
           </div>
         </div>
         <div class="col-12 col-md-6" v-show="showEndDate">
           <div class="form-group end-date">
-            <label for="endDate">{{ $t('book.form.titles.finished') }}</label>
-            <date-input :year.sync="book.endDateYear" :month.sync="book.endDateMonth" :day.sync="book.endDateDay"></date-input>
+            <label for="endDate">{{ $t("book.form.titles.finished") }}</label>
+            <date-input
+              :year.sync="book.endDateYear"
+              :month.sync="book.endDateMonth"
+              :day.sync="book.endDateDay"
+            ></date-input>
           </div>
         </div>
       </div>
       <div class="form-group">
-        <label for="note">{{ $t('book.form.titles.notes') }}</label>
-        <textarea type="text" name="note" id="note" class="form-control" rows="5" v-model.trim="book.note"></textarea>
+        <label for="note">{{ $t("book.form.titles.notes") }}</label>
+        <textarea
+          type="text"
+          name="note"
+          id="note"
+          class="form-control"
+          rows="5"
+          v-model.trim="book.note"
+        ></textarea>
       </div>
       <div class="form-group">
-        <button class="btn btn-primary w-100" type="submit">{{ $t('book.actions.save') }}</button>
+        <button class="btn btn-primary w-100" type="submit">
+          {{ $t("book.actions.save") }}
+        </button>
       </div>
     </form>
   </div>
@@ -126,37 +182,74 @@
 
 <script>
 import bookMixin from "@/mixins/book-form-mixin";
-import DateInput from '@/components/inputs/BookDateInput.vue';
-import AudioBookUnitsInput from '@/components/inputs/AudioBookUnitsInput.vue';
-import { 
+import DateInput from "@/components/inputs/BookDateInput.vue";
+import AudioBookUnitsInput from "@/components/inputs/AudioBookUnitsInput.vue";
+import {
   BOOK_UPDATE_ACTION,
   BOOK_GET_BY_GUID_ACTION,
   NOTIFICATION_SUCCESS_ACTION,
   NOTIFICATION_DANGER_ACTION,
 } from "@/store/naming";
 import store from "@/store";
+import { DONE_STATUS } from "@/models/book";
 
 export default {
   components: {
     DateInput,
     AudioBookUnitsInput,
   },
+  data: () => ({
+    initialStatus: null,
+    initialUnits: null,
+  }),
   mixins: [bookMixin],
   methods: {
     submit(event) {
-      this.$store.dispatch(BOOK_UPDATE_ACTION, this.book).then(() => {
-        this.redirectForBook(this.book);
-        this.$store.dispatch(NOTIFICATION_SUCCESS_ACTION, this.$t('book.notification.update.success'));
-        this.$forceUpdate();
-      }).catch(() => this.$store.dispatch(NOTIFICATION_DANGER_ACTION, this.$t('book.notification.update.fail')));
+      this.$store
+        .dispatch(BOOK_UPDATE_ACTION, this.book)
+        .then(() => {
+          this.redirectForBook(this.book);
+          this.$store.dispatch(
+            NOTIFICATION_SUCCESS_ACTION,
+            this.$t("book.notification.update.success")
+          );
+          this.$forceUpdate();
+        })
+        .catch(() =>
+          this.$store.dispatch(
+            NOTIFICATION_DANGER_ACTION,
+            this.$t("book.notification.update.fail")
+          )
+        );
 
       event.preventDefault();
+    },
+    statusChange() {
+      if (this.initialStatus == DONE_STATUS) return;
+
+      if (this.book.status === DONE_STATUS) {
+        this.book.endDateYear = new Date().getFullYear();
+        this.book.endDateMonth = new Date().getMonth() + 1;
+        this.book.endDateDay = new Date().getDate();
+
+        this.book.doneUnits = this.book.totalUnits;
+      } else {
+        this.book.endDateYear = null;
+        this.book.endDateMonth = null;
+        this.book.endDateDay = null;
+
+        this.book.doneUnits = this.initialUnits;
+      }
     },
   },
   beforeRouteEnter(to, from, next) {
     const bookGuid = to.params.guid;
     store.dispatch(BOOK_GET_BY_GUID_ACTION, bookGuid).then((book) => {
-      next((vm) => (vm.book = Object.assign({}, book)));
+      next((vm) => {
+        vm.book = Object.assign({}, book);
+        vm.initialStatus = book.status;
+        vm.initialUnits = book.doneUnits;
+      });
     });
   },
 };
--- frontend.v2/tests/unit/components/books/forms/book-update.spec.js
+++ frontend.v2/tests/unit/components/books/forms/book-update.spec.js
@@ -7,8 +7,9 @@ import {
 import BookUpdate from '@/views/books/entity/BookUpdate.vue';
 
 describe('BookUpdate.vue', () => {
-    it('Renders dates', async () => {
-        const wrapper = shallowMount(BookUpdate, {
+    let wrapper = null;
+    beforeEach(async () => {
+        wrapper = shallowMount(BookUpdate, {
             mocks: {
                 $t: () => '',
                 $route: {
@@ -18,7 +19,9 @@ describe('BookUpdate.vue', () => {
                 }
             }
         });
+    })
 
+    it('Renders dates', async () => {
         wrapper.vm.book.status = 0;
 
         await wrapper.vm.$nextTick();
@@ -39,5 +42,54 @@ describe('BookUpdate.vue', () => {
 
         expect(wrapper.find('.start-date').isVisible()).equal(true);
         expect(wrapper.find('.end-date').isVisible()).equal(true);
+    })
+
+    it('Prefill dates for new done status', async () => {
+        wrapper.vm.initialStatus = 1;
+        wrapper.vm.initialUnits = 10;
+
+        wrapper.vm.book.totalUnits = 100;
+        wrapper.vm.book.doneUnits = 10;
+
+        expect(wrapper.vm.book.endDateYear).equal(undefined);
+        expect(wrapper.vm.book.endDateMonth).equal(undefined);
+        expect(wrapper.vm.book.endDateDay).equal(undefined);
+        expect(wrapper.vm.book.doneUnits).equal(10);
+
+        wrapper.find('#status').setValue(2);
+
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.book.endDateYear).equal(new Date().getFullYear());
+        expect(wrapper.vm.book.endDateMonth).equal(new Date().getMonth() + 1);
+        expect(wrapper.vm.book.endDateDay).equal(new Date().getDate());
+        expect(wrapper.vm.book.doneUnits).equal(100);
+
+        wrapper.find('#status').setValue(1);
+
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.book.endDateYear).equal(null);
+        expect(wrapper.vm.book.endDateMonth).equal(null);
+        expect(wrapper.vm.book.endDateDay).equal(null);
+        expect(wrapper.vm.book.doneUnits).equal(10);
+    })
+
+    it('Prefill dates for new done status', async () => {
+        wrapper.vm.initialStatus = 2;
+
+        expect(wrapper.vm.book.endDateYear).equal(undefined);
+        expect(wrapper.vm.book.endDateMonth).equal(undefined);
+        expect(wrapper.vm.book.endDateDay).equal(undefined);
+        expect(wrapper.vm.book.doneUnits).equal(undefined);
+
+        wrapper.find('#status').setValue(2);
+
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.book.endDateYear).equal(undefined);
+        expect(wrapper.vm.book.endDateMonth).equal(undefined);
+        expect(wrapper.vm.book.endDateDay).equal(undefined);
+        expect(wrapper.vm.book.doneUnits).equal(undefined);
     })
 })
