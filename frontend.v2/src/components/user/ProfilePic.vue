cbce1b6ebe3530b4b6cb0a5f8b91f9f83ff514cf
Alex Shakirov <a.shakirov@infinnity.ru>
2020-11-06 13:33:16 +0500

    fixed avatar drawing

--- frontend.v2/src/components/user/ProfilePic.vue
+++ frontend.v2/src/components/user/ProfilePic.vue
@@ -1,6 +1,20 @@
 <template functional>
-    <img class="avatar" v-if="props.avatar" :src="props.avatar" />
+    <div class="profile-pic" v-if="props.avatar" :style="{ 'background-image': `url(\'${props.avatar}\')`}"> </div>
     <div v-else class="icon-place">
         <person-icon></person-icon>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .profile-pic {
+        background-origin: content-box;
+
+        background-position: 50% 50%;
+        background-size: 100% auto;
+
+        height: 100%;
+        width: 100%;
+
+        border-radius: 50%;
+    }
+</style>
