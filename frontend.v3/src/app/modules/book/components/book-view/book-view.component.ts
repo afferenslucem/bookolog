161cbd1cd33fa4c337757f01271762e4efe50bc3
Alex Shakirov <a.shakirov@infinnity.ru>
2020-11-24 11:57:14 +0500

    wip

--- frontend.v3/src/app/modules/book/components/book-view/book-view.component.ts
+++ frontend.v3/src/app/modules/book/components/book-view/book-view.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 import { filter, map, takeUntil } from 'rxjs/operators';
 import { TitleService } from '../../../ui/service/title.service';
@@ -17,7 +17,7 @@ import { BookDeleteDialogComponent, DeleteDialogResult } from '../book-delete-di
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookViewComponent implements OnInit, OnDestroy {
-  public book$: Observable<Book> = new Observable<Book>();
+  public book: Book;
 
   public BookType: typeof BookType = BookType;
 
@@ -25,14 +25,17 @@ export class BookViewComponent implements OnInit, OnDestroy {
 
   public destroy$ = new Subject();
 
-  constructor(private activatedRoute: ActivatedRoute, public titleService: TitleService, public dialog: MatDialog, private bookService: BookService) {
-    this.book$ = activatedRoute.data.pipe(
-      map(data => data.book)
-    );
+  constructor(private activatedRoute: ActivatedRoute,
+              public titleService: TitleService,
+              public dialog: MatDialog,
+              private bookService: BookService,
+              private router: Router
+  ) {
+    activatedRoute.data.subscribe(data => this.book = data.book);
   }
 
   ngOnDestroy(): void {
-      this.destroy$.next();
+    this.destroy$.next();
   }
 
   ngOnInit(): void {
@@ -47,7 +50,7 @@ export class BookViewComponent implements OnInit, OnDestroy {
     }
   }
 
-  public openDeleteDialog(book: Book): void {
+  public async openDeleteDialog(book: Book): Promise<void> {
     const dialogRef = this.dialog.open(BookDeleteDialogComponent, {
       width: '90%',
       maxWidth: '300px',
@@ -56,10 +59,30 @@ export class BookViewComponent implements OnInit, OnDestroy {
     dialogRef.afterClosed().pipe(
       filter((item?: DeleteDialogResult) => item && item === 'delete'),
       takeUntil(this.destroy$),
-    ).subscribe(() => this.deleteBook(book));
+    ).subscribe(async () => {
+      await this.deleteBook(book);
+      await this.redirect();
+    });
   }
 
-  public deleteBook(book: Book): void {
-    this.bookService.deleteBook(book);
+  public async deleteBook(book: Book): Promise<void> {
+    await this.bookService.deleteBook(book);
+  }
+
+  public async redirect(): Promise<void> {
+    switch (this.book.status) {
+      case BookStatus.InProgress: {
+        await this.router.navigate(['/in-progress']);
+        break;
+      }
+      case BookStatus.Done: {
+        await this.router.navigate(['/done']);
+        break;
+      }
+      case BookStatus.ToRead: {
+        await this.router.navigate(['/to-read']);
+        break;
+      }
+    }
   }
 }
