6069505de8264fa1d542a496a0cde77cd582240e
Alex Shakirov <a.shakirov@infinnity.ru>
2021-01-28 17:06:23 +0500

    feat: add gzipping of client

--- frontend.v3/src/environments/environment.prod.ts
+++ frontend.v3/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   serverUrl: 'https://api.hrodvitnir.pw',
   restoreTimeSeconds: 12 * 60 * 60,
-  version: '0.3.6',
+  version: '0.3.7',
   filePath: '/file/',
   notificationCloseTime: 3000,
 };
--- frontend.v3/src/environments/environment.ts
+++ frontend.v3/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   serverUrl: 'http://localhost:45080',
   restoreTimeSeconds: 60,
-  version: '0.3.6',
+  version: '0.3.7',
   filePath: '/assets/',
   notificationCloseTime: 3000,
 };
