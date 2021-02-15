853fc37b5a3f88c29dd56e25b96ea397412d13a4
Alex Shakirov <a.shakirov@infinnity.ru>
2021-02-15 15:50:36 +0500

    task: updated uuid lib

--- frontend.v3/src/environments/environment.ts
+++ frontend.v3/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   serverUrl: 'http://localhost:5000',
   restoreTimeSeconds: 60,
-  version: '0.3.29',
+  version: '0.3.30',
   filePath: '/assets/',
   notificationCloseTime: 3000,
 };
