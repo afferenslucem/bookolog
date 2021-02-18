bffd7580a67f29b518374dfba6222c732b8fa545
Alex Shakirov <a.shakirov@infinnity.ru>
2021-02-18 17:25:26 +0500

    task: updated version

--- frontend.v3/src/environments/environment.prod.ts
+++ frontend.v3/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   serverUrl: '/api',
   restoreTimeSeconds: 12 * 60 * 60,
-  version: '0.4.2',
+  version: '0.4.3',
   filePath: '/file/',
   notificationCloseTime: 3000,
 };
