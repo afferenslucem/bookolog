b8c7968f5121d0762321733f20615e6dca4a5bac
Hrodvitnir <alexshakirov74@gmail.com>
2021-02-24 10:17:20 +0500

    check tests

--- frontend.v3/src/app/modules/collection/services/collection.storage.service.spec.ts
+++ frontend.v3/src/app/modules/collection/services/collection.storage.service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CollectionStorageService } from './collection.storage.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from '../../user/services/user.service';
 
 describe('CollectionStorageService', () => {
   let service: CollectionStorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: UserService, useValue: {
+            user: {
+              login: 'hrodvitnir'
+            }
+          }
+        }
+      ],
+      imports: [
+        HttpClientTestingModule,
+      ]
+    });
     service = TestBed.inject(CollectionStorageService);
   });
 
