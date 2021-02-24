b1a144fc125260709d98e68642097db3e04286fe
Alex Shakirov <alexshakirov74@gmail.com>
2021-02-24 12:55:22 +0500

    added test for pipes

--- frontend.v3/src/app/modules/formatting/pipes/choose-form.pipe.ts
+++ frontend.v3/src/app/modules/formatting/pipes/choose-form.pipe.ts
@@ -8,7 +8,7 @@ export class ChooseFormPipe implements PipeTransform {
     const lostBy100 = count % 100;
     const lostBy10 = count % 10;
 
-    if (lostBy100 < 20 && lostBy100 > 10 || lostBy10 > 4) {
+    if (lostBy100 < 20 && lostBy100 > 10 || lostBy10 > 4 || lostBy10 === 0) {
       return label3;
     } else if (lostBy10 === 1) {
       return label1;
