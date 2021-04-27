53e6233dbdd7e9b806d133831cea11c71fedb7d2
Alex Shakirov <a.shakirov@infinnity.ru>
2021-04-27 11:45:22 +0500

    feat: Иконки переведены на prefetch
    Closes #116

--- frontend.v3/src/app/modules/list-module/components/empty-list-icon/empty-list-icon.component.ts
+++ frontend.v3/src/app/modules/list-module/components/empty-list-icon/empty-list-icon.component.ts
@@ -1,84 +1,26 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { LocalStorageService } from '../../../../main/services/local-storage.service';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empty-list-icon',
   templateUrl: './empty-list-icon.component.html',
   styleUrls: ['./empty-list-icon.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmptyListIconComponent implements OnInit {
-  public id: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-  public retries = 0;
   public isFailed = false;
+  public id = 0;
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.id.next(this.getRandomIndex(148));
-  }
-
-  public onLoad(): void {
-    this.appendKnownIcon();
+    this.id = this.getRandomIndex(17);
   }
 
   public onError(): void {
-    this.retries++;
-
-    if (this.retries > 2) {
-      this.isFailed = true;
-      return;
-    }
-
-    const set = this.readIconSet();
-    this.id.next(this.chooseRandomIcon(set));
+    this.isFailed = true;
   }
 
-  public appendKnownIcon(): void {
-    const set = this.readIconSet();
-    set.add(this.id.value);
-
-    this.writeIconSet(set);
-  }
-
-  public get iconFullName(): Observable<string> {
-    return this.id.pipe(map(item => `/assets/empty-list-icons/${item}.svg`));
-  }
-
-  public readIconSet(): Set<number> {
-    const icons = this.localStorageService.getItem('knownIcons');
-
-    if (icons) {
-      const set = new Set<number>(JSON.parse(icons));
-
-      return set;
-    } else {
-      return new Set<number>();
-    }
-  }
-
-  public setToArray(set: Set<number>): number[] {
-    return Array.from(set);
-  }
-
-  public writeIconSet(set: Set<number>): void {
-    const array = this.setToArray(set);
-
-    const json = JSON.stringify(array);
-
-    this.localStorageService.setItem('knownIcons', json);
-  }
-
-  public chooseRandomIcon(set: Set<number>): number {
-    if (set.size === 0) return null;
-
-    const index = this.getRandomIndex(set.size);
-
-    const array = this.setToArray(set);
-
-    return array[index];
+  public get iconFullName(): string {
+    return `/assets/empty-list-icons/${this.id}.svg`;
   }
 
   private getRandomIndex(bound: number): number {
