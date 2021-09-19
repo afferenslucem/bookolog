c1cd83b035c19cd949511f89ab12b1c00827751c
Alex Shakirov <alexshakirov74@gmail.com>
2021-09-19 09:52:07 +0500

    fix build

--- frontend.v3/src/app/modules/book/components/book-reread-form/book-reread-form.component.ts
+++ frontend.v3/src/app/modules/book/components/book-reread-form/book-reread-form.component.ts
@@ -47,7 +47,7 @@ export class BookRereadFormComponent extends AbstractBookDataForm implements OnI
 
     this.bookForm.progressType = this.progressAlgorithmService.getAlgorithm(this.bookForm.type);
 
-    this.bookForm.typeChanges.subscribe(() => this.onTypeChange());
+    this.bookForm.typeChanges.subscribe((type) => this.onTypeChange(type));
   }
 
   public ngOnInit(): void {}
