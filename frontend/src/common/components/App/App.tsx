09e2665dfc2479563d20daa0622294ff269654a6
Alex Shakirov <a.shakirov@infinnity.ru>
2022-11-03 13:56:53 +0500

    added drawer

--- frontend/src/common/components/App/App.tsx
+++ frontend/src/common/components/App/App.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.scss';
 import { Route, Routes } from "react-router-dom";
 import Auth from "../../../pages/Auth/Auth";
-import Layout from "../Layout/Layout";
 import Account from "../../../pages/Account/Account";
 
 function App() {
-  return (
-      <Routes>
-          <Route element={<Layout />}>
-              <Route path="/auth/*" element={<Auth />} />
-              <Route path="/*" element={<Account />} />
-          </Route>
-      </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/auth/*" element={<Auth/>}/>
+            <Route path="/*" element={<Account/>}/>
+        </Routes>
+    );
 }
 
 export default App;
--- frontend/src/pages/Account/Account.tsx
+++ frontend/src/pages/Account/Account.tsx
@@ -1,10 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import InProgress from "./pages/InProgress/InProgress";
+import { AppBar, Container, Drawer, IconButton, Toolbar } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useReducer } from "react";
 
 export default function Account() {
+    const [isDrawerOpened, toggleDrawer] = useReducer(flag => !flag, false);
+
     return (
-        <Routes>
-            <Route path="/in-progress" element={<InProgress/>}/>
-        </Routes>
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton size="large" edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+                        <MenuIcon/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="md">
+                <Routes>
+                    <Route path="/in-progress" element={<InProgress/>}/>
+                </Routes>
+            </Container>
+            <Drawer anchor="bottom" open={isDrawerOpened} onClose={toggleDrawer}>
+                Hello
+            </Drawer>
+        </div>
     )
 }
