6140147a4f9335bdced56b0badb662e921783a7e
Alex Shakirov <a.shakirov@infinnity.ru>
2022-11-04 11:34:05 +0500

    added loading

--- frontend/src/pages/Account/pages/InProgress/InProgress.tsx
+++ frontend/src/pages/Account/pages/InProgress/InProgress.tsx
@@ -1,24 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { BookLoader } from "../../../../common/utils/book-loader";
 import { Book as BookModel } from "../../../../common/models/book/book";
 import { useOnInit } from "../../../../common/utils/hooks";
 import Book from "../../components/Book/Book";
 
 import './InProgress.scss';
+import { CircularProgress } from "@mui/material";
 
 export default function InProgress() {
     const [loading, setLoading] = useState(true);
     const [books, setBooks] = useState<BookModel[]>([]);
 
     useOnInit(() => {
+        setLoading(true);
         new BookLoader().getBooksInProgress()
             .then(books => setBooks(books))
+            .finally(() => setLoading(false));
     })
 
     return (
         <div className="in-progress">
             {
-                books.map(book => <Book key={book.guid} book={book} />)
+                loading
+                    ? <div className="in-progress__loading">
+                        <CircularProgress size="6rem"/>
+                      </div>
+                    : books.map(book => <Book key={book.guid} book={book}/>)
             }
         </div>
     )
